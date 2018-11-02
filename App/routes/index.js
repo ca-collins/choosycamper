153272645a5861a9acb7674103b70a304d82ad1a
Chris <cacollins08@gmail.com>
2018-11-02 16:22:19 +0000

    minor updates to verbage

--- App/routes/index.js
+++ App/routes/index.js
@@ -24,7 +24,7 @@ router.post("/register", function(req, res) {
             return res.redirect("/register");
         }
         passport.authenticate("local")(req, res, function(){
-            req.flash("success", "Welcome to YelpCamp, " + user.username +"!!!");
+            req.flash("success", "Welcome to ChoosyCamper, " + user.username +"!!!");
             res.redirect("/campgrounds");
         });
     });
